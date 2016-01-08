a01831d5537dad53c388456cb3232ff8c74c6ed1
Asai Masami <shakeforme@gmail.com>
2016-01-08 16:06:40 +0900

    initial commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+ruby "2.3.0"
+
+gem "sinatra"
+gem "sinatra-contrib"
+gem "amazon-ecs"
+gem "slim"
+
+group :developemnt do
+end
---
+++ app.rb
@@ -0,0 +1,82 @@
+require 'sinatra'
+require 'amazon/ecs'
+require 'sinatra/reloader' if development?
+
+class AmazonError < StandardError;end
+
+before do
+  Amazon::Ecs.configure do |options|
+    options[:AWS_access_key_id] = ENV['PAA_AWS_ID']
+    options[:AWS_secret_key]    = ENV['PAA_AWS_SECRET']
+    options[:associate_tag]     = 'count-0_22'
+    options[:response_group]    = 'Medium'
+  end
+end
+
+helpers do
+  def lookup(asin)
+    begin
+      response = Amazon::Ecs.item_lookup(asin, country: 'jp') 
+      raise AmazonError if response.has_error?
+    rescue Amazon::RequestError => e
+      puts e
+      puts "Retry request to amazon"
+      sleep 1.0
+      retry
+    else
+      return response.first_item
+    end
+  end
+
+  def search(keyword, search_index = 'All', page = 1)
+    begin
+      response = Amazon::Ecs.item_search(keyword,
+                                         search_index: search_index,
+                                         item_page: page,
+                                         country: 'jp')
+      raise AmazonError if response.has_error?
+    rescue Amazon::RequestError => e
+      puts e
+      puts "Retry request to amazon"
+      sleep 1.0
+      retry
+    else
+      return response
+    end
+  end
+end
+
+%w(/ /search /search/?).each do |path|
+  get path do
+    slim :index
+  end
+end
+
+get '/style.css' do
+  scss :style
+end
+
+get '/search/:keyword' do
+  keyword = params[:keyword]
+  search_index = params[:search_index] || 'All'
+  page = params[:page] || 1
+
+  res = search(keyword, search_index, page)
+
+  @items = res.items
+  @total_resulsts = res.total_results
+  @total_pages = res.total_pages
+  @page = page
+  
+  if request.xhr?
+    slim :search_result, layout: false
+  else
+    slim :search
+  end
+end
+
+get '/asin/:asin' do
+  halt 400, 'エラー!ASINは10桁の番号です' if params[:asin].size != 10
+  @item = lookup(params[:asin])
+  slim :asin
+end
